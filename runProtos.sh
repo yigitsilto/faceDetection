2f47c0802457fdb38e57f7f3d9654fafee7d6a58
yigitsilto <47320854+yigitsilto@users.noreply.github.com>
2023-10-07 15:51:49 +0300

    docker fix

--- runProtos.sh
+++ runProtos.sh
@@ -1,5 +1,5 @@
-docker-compose down
-docker-compose up -d
+docker stop $(docker ps -aq)
+docker-compose up --build -d
 export PATH=$PATH:$HOME/go/bin
 export PATH=$PATH:/usr/local/go/bin
 protoc -I proto --go_out=./proto-output --go-grpc_out=./proto-output ./proto/*.proto
